91cbaafb46cbc89841f9387c6db58db41148de50
haashim007 <102474562+haashim007@users.noreply.github.com>
2022-08-28 21:15:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,15 +382,23 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://du-link.in/api'
-    params = {'api': '6bcee0de02a5e5de87efd553c736f3c12fe37a4c',
-              'url': link,
+
+    url = f'https://api.shareus.in/shortLink'
+    params = {'api': 'dXlx5jBbbdTGCXCNw4GKaIVWpgx1',
+              'link': link,
+              'format': 'json'
               }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json()
-            if data["status"] == "success":
-                return data['shortenedUrl']
-            else:
-                return f"Error: {data['message']}"
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json(content_type='text/html')
+                if data["status"] == "success":
+                    return data['shortlink']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'https://api.shareus.in/directLink?token={SHORTENER_API}&link={link}'
