de6f52f8ffd18923d3561b368a71118ea70f6936
DRAGON-PRO <116484443+DRAGON-PRO@users.noreply.github.com>
2022-11-06 00:05:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -477,9 +477,9 @@ async def get_shortlink(link):
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://shorturllink.in/api?api={SHORTENER_API}&link={link}'
+                    return f'https://shorturllink.in/api?'
 
 
     except Exception as e:
         logger.error(e)
-        return f'https://shorturllink.in/api?api={SHORTENER_API}&link={link}'
+        return f'https://shorturllink.in/api?'
