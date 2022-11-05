0ae16e95e2840e79be1f37b04a8226b2c9c3db3b
DRAGON-PRO <116484443+DRAGON-PRO@users.noreply.github.com>
2022-11-05 19:20:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/haashim0/url.git /EvaMaria
+  git clone https://github.com/DRAGON-PRO/url.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
